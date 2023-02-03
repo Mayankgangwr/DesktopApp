c0cd4b507b4a4153f0a3f5c5ba618fd64e7af112
Mayank Gangwar <iammayankgangwarbly@gmail.com>
2023-02-03 05:32:49 +0000

    Update 8 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
-import "./styles.css";
+import { useState } from "react";
 
-export default function App() {
+import Chart from "react-apexcharts";
+import useAlphaVantage from "./useAlphaVantage";
+
+import { symbols } from "./api";
+
+import "./App.css";
+
+function App() {
+  const [symbol, setSymbol] = useState("MSFT");
+
+  const dataAlpha = useAlphaVantage(`?symbol=${symbol}`);
+
+  if (!dataAlpha) return <div className="loading">Loading...</div>;
+
+  const state = {
+    series: [
+      {
+        data: dataAlpha,
+      },
+    ],
+    options: {
+      chart: {
+        type: "candlestick",
+        height: 350,
+      },
+      title: {
+        text: "Candlestick Chart",
+        align: "left",
+      },
+      xaxis: {
+        type: "datetime",
+      },
+      yaxis: {
+        tooltip: {
+          enabled: true,
+        },
+      },
+    },
+  };
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <div>
+        <select
+          className="select"
+          value={symbol}
+          onChange={(e) => {
+            setSymbol(e.target.value);
+          }}
+        >
+          {symbols.map((symbol) => (
+            <option key={symbol} value={symbol}>
+              {symbol}
+            </option>
+          ))}
+        </select>
+      </div>
+      <Chart
+        options={state.options}
+        series={state.series}
+        type="candlestick"
+        height={350}
+      />
     </div>
   );
 }
+
+export default App;
